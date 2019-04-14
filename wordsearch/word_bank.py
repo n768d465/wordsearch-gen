ebb5fb79c545d192daccc9138d6596b9017664a0
Neil Dunn <ndunn178@gmail.com>
2019-04-13 19:34:56 -0500

    more efficient use of memory

--- wordsearch/word_bank.py
+++ wordsearch/word_bank.py
@@ -1,43 +1,25 @@
 import random
 import requests
-from collections import defaultdict
-import json
 
 WORD_LIST = (
-    "https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json"
+    "http://svnweb.freebsd.org/csrg/share/dict/words?view=co&content-type=text/plain"
 )
 
 
-def pull_word(word_list):
-    pulled_word = random.sample(word_list, 1)[0]
-    word_list.remove(pulled_word)
-    return pulled_word
+def sample_words(max_length):
+    response = get_words_from_site(max_length)
+    min_word_length = 3
+    total_words = 0
+    word_range = range(min_word_length, max_length + 1)
 
-
-def get_wordbank(max_length):
-    word_list = get_local_words(max_length)
-    all_words = defaultdict(lambda: [])
-
-    for w in word_list:
-        if len(w) <= max_length:
-            all_words[len(w)].append(w)
-
-    return all_words
+    while True:
+        w = str(random.choice(response), "UTF-8").lower()
+        if "'" in w or len(w) not in word_range:
+            continue
+        else:
+            yield w
+            total_words += 1
 
 
 def get_words_from_site(max_length):
-    response = requests.get(WORD_LIST)
-    words = set()
-    min_word_length = 3
-    word_range = range(min_word_length, max_length + 1)
-    for word in response.content.splitlines():
-        if len(word) in word_range:
-            words.add(word)
-
-    return words
-
-
-def get_local_words(max_length):
-    with open("words_dictionary.json") as outf:
-        words = json.load(outf)
-        return set(words.keys())
+    return requests.get(WORD_LIST).content.splitlines()
--- wordsearch/wordsearch_generator.py
+++ wordsearch/wordsearch_generator.py
@@ -1,10 +1,13 @@
-from word_bank import get_wordbank, pull_word, get_local_words
-from random import sample, choice
+from word_bank import sample_words
+from random import choice
 from string import ascii_lowercase
 import argparse
 
 
 def is_placeable(word, path):
+    if len(path) < len(word):
+        return False
+
     return not any((step != " " and step != char) for (char, step) in zip(word, path))
 
 
@@ -31,11 +34,6 @@ class WordSearchGenerator:
             elif orientation == "DIAGONAL":
                 self.grid[i][i] = char
 
-    def _get_new_positions(self, orientation):
-        i, j = sample(self.coords, 1)[0]
-        self.coords.remove((i, j))
-        return i, j
-
     def _get_path(self, orientation, row, col):
         if orientation == "HORIZONTAL":
             return [self.grid[row][i] for i in range(col, self.dim)]
@@ -50,28 +48,24 @@ class WordSearchGenerator:
             [choice(ascii_lowercase) if y == " " else y for y in x] for x in self.grid
         ]
 
-    def _placeable_words(self, path, path_len):
-        possible_words = {word for w in range(3, path_len) for word in self.words[w]}
-        return {word for word in possible_words if is_placeable(word, path)}
+    def make_wordsearch(self):
+        if self.max_word_length > self.dim:
+            raise ValueError("Max word length is larger than the grid size.")
+        for word in sample_words(self.max_word_length):
+            ort = choice(("HORIZONTAL", "VERTICAL", "DIAGONAL"))
+            try:
+                i, j = choice(self.coords)
+            except IndexError:
+                break
+            self.coords.remove((i, j))
+            path = self._get_path(ort, i, j)
 
-    def _make_wordsearch(self):
-        for _ in range(self.dim):
-            placement_attempts = self.dim
-            while placement_attempts > 0:
-                ort = choice(("HORIZONTAL", "VERTICAL", "DIAGONAL"))
-                i, j = self._get_new_positions(ort)
-                path = self._get_path(ort, i, j)
-                path_len = len(path)
-
-                placeable_words = self._placeable_words(path, path_len)
-                try:
-                    word = pull_word(placeable_words)
-                except Exception:
-                    placement_attempts = placement_attempts - 1
-                    continue
+            if is_placeable(word, path):
                 self._place_word(ort, word, i, j)
                 self.bank.add(word)
-                break
+                print("Placeable:", word, path)
+            else:
+                continue
 
         if self.fill:
             self._fill_remaining_spaces()
@@ -82,18 +76,22 @@ class WordSearchGenerator:
         self.max_word_length = max_word_length
         self.grid = [[" " for y in range(x * dim, x * dim + dim)] for x in range(dim)]
         self.coords = [(x, y) for x in range(self.dim) for y in range(self.dim)]
-        self.words = get_wordbank(self.max_word_length) if not len(words) else words
         self.bank = set()
-        self._make_wordsearch()
 
 
 def run_wordsearch(dim, fill, length):
     ws = WordSearchGenerator(dim=dim, fill=fill, max_word_length=length)
+    ws.make_wordsearch()
 
+    print()
     for row in ws.grid:
         print(" ".join(row))
 
-    print(ws.bank)
+    print("\nWord bank")
+    print("---------")
+    for word in ws.bank:
+        print(word, end=" ")
+    print()
 
 
 if __name__ == "__main__":
@@ -104,6 +102,4 @@ if __name__ == "__main__":
 
     args = parser.parse_args()
 
-    if args.max_word_length < 3:
-        raise ValueError("Word size too small, use words 3 letters or longer.")
     run_wordsearch(args.dim, args.no_fill, args.max_word_length)
