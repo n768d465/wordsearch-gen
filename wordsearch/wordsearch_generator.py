96efc305b43afc56bb318dc5dd6a595bec106f70
Neil Dunn <ndunn178@gmail.com>
2020-02-14 18:44:54 -0600

    fix indices

--- wordsearch/wordsearch_generator.py
+++ wordsearch/wordsearch_generator.py
@@ -25,11 +25,11 @@ class WordSearchGenerator:
                 self._path_positions.append((k, j))
         if orientation == "DIAGONAL":
             for x, y in zip(range(i, grid_len), range(j, grid_len)):
-                self._current_path.append(self.grid[i][j])
+                self._current_path.append(self.grid[x][y])
                 self._path_positions.append((x, y))
         if orientation == "FORWARD DIAGONAL":
             for x, y in zip(range(i, -1, -1), range(j, grid_len)):
-                self._current_path.append(self.grid[i][j])
+                self._current_path.append(self.grid[x][y])
                 self._path_positions.append((x, y))
 
     def _fill_remaining_spaces(self):
