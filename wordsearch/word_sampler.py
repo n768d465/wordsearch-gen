5cd999d8e40b579ab4c50a913ea0b8922b1e5a55
Neil Dunn <ndunn178@gmail.com>
2021-12-11 21:52:14 -0600

    The Great Refactor

--- wordsearch/word_sampler.py
+++ wordsearch/word_sampler.py
@@ -1,22 +1,15 @@
 import random
-from masterlist.builder import get_master_list
 
 
-class Sampler:
-    def __call__(self, path):
-        choice = str(random.choice(self.word_range))
-        sample = [WordItem(w) for w in self.word_list[self.category].get(choice, [])]
-        placeables = list(filter(lambda w: w and _filter_criteria(w, path), sample))
+def sample_placeable_word(category_list, min_length, max_length, path):
+    choice = str(random.choice(range(min_length, max_length)))
+    sample = [WordItem(w) for w in category_list.get(choice, [])]
+    placeables = list(filter(lambda w: w and _filter_criteria(w, path), sample))
 
-        try:
-            return random.choice(placeables)
-        except Exception:
-            return None
-
-    def __init__(self, category, min_length, max_length):
-        self.category = category
-        self.word_range = range(min_length, max_length + 1)
-        self.word_list = get_master_list()
+    try:
+        return random.choice(placeables)
+    except IndexError:
+        return None
 
 
 class WordItem(object):
@@ -40,8 +33,4 @@ def _filter_criteria(word_item: WordItem, path: "list[str]") -> bool:
     if len(word) > len(path):
         return False
 
-    return _is_placeable(word, path)
-
-
-def _is_placeable(word, path) -> bool:
     return not any((step != " " and step != char) for (char, step) in zip(word, path))
--- wordsearch/wordsearch_generator.py
+++ wordsearch/wordsearch_generator.py
@@ -1,66 +1,51 @@
-from .word_sampler import Sampler
+from .word_sampler import sample_placeable_word
 from random import choice, randint
 from string import ascii_lowercase
 from .path import path
+from masterlist.builder import get_master_list
 
 
 class WordSearchGenerator:
-    def _place_word(self, word_item):
-        for (i, j), char in zip(self._path_positions, word_item.word):
-            self.grid[i][j] = char
-            word_item.positions.append((i, j))
+    def make_wordsearch(self, category, dim, min_word_length, max_word_length):
+        self.grid = [[" "] * dim for _ in range(dim)]
+        self.bank = set()
+        self.ws_data = []
 
-    def _form_current_path(self, orientation, i, j):
-        self._current_path = []
-        self._path_positions = []
+        max_words = randint(dim - 2, dim + 2)
 
-        for x, y, char in path(self.grid, orientation, i, j):
-            self._current_path.append(char)
-            self._path_positions.append((x, y))
+        while len(self.bank) < max_words:
+            _current_path = []
+            _path_positions = []
+            orientation = choice(
+                ("HORIZONTAL", "VERTICAL", "DIAGONAL", "FORWARD DIAGONAL")
+            )
 
-    def _fill_remaining_spaces(self):
-        self.grid_words_only = [[r for r in row] for row in self.grid]
+            i, j = (randint(0, dim - 1), randint(0, dim - 1))
+
+            for x, y, char in path(self.grid, orientation, i, j):
+                _current_path.append(char)
+                _path_positions.append((x, y))
+
+            word_item = sample_placeable_word(
+                self.word_list[category],
+                min_word_length,
+                max_word_length,
+                _current_path,
+            )
+
+            if word_item and word_item.word2 not in self.bank:
+                for (i, j), char in zip(_path_positions, word_item.word):
+                    self.grid[i][j] = char
+                    word_item.positions.append((i, j))
+                self.bank.add(word_item.word2)
+                self.ws_data.append(word_item)
+
         self.grid = [
             [choice(ascii_lowercase) if y == " " else y for y in x] for x in self.grid
         ]
 
-    def _clear_params(self):
-        self.grid = [[" "] * self.dim for _ in range(self.dim)]
-        self.grid_words_only = [[]]
+    def __init__(self):
+        self.grid = []
         self.bank = set()
         self.ws_data = []
-
-    def make_wordsearch(self):
-        self._clear_params()
-        self.max_words = randint(self.dim - 2, self.dim + 2)
-        self.sample_placeable_word.word_range = range(
-            self.min_word_length, self.max_word_length
-        )
-        self.sample_placeable_word.category = self.category
-
-        while len(self.bank) < self.max_words:
-            ort = choice(("HORIZONTAL", "VERTICAL", "DIAGONAL", "FORWARD DIAGONAL"))
-            i, j = (randint(0, self.dim - 1), randint(0, self.dim - 1))
-
-            self._form_current_path(ort, i, j)
-            word_item = self.sample_placeable_word(self._current_path)
-            if word_item and word_item.word not in self.bank:
-                self._place_word(word_item)
-                self.bank.add(word_item.word2)
-                self.ws_data.append(word_item)
-
-        self._fill_remaining_spaces()
-
-    def __init__(self, category="", dim=10, min_word_length=3, max_word_length=7):
-        self.category = category
-        self.dim = dim
-        self.min_word_length = min_word_length
-        self.max_word_length = max_word_length
-        self.grid = [[]]
-        self.grid_words_only = [[]]
-        self.bank = set()
-        self.ws_data = []
-        self.max_words = randint(self.dim - 2, self.dim + 2)
-        self.sample_placeable_word = Sampler(category, min_word_length, max_word_length)
-        self._current_path = []
-        self._path_positions = []
+        self.word_list = get_master_list()
